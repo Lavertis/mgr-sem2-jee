27cbaefc25520ec500b0d0be4902cab914d22920
lavertis <29688908+Lavertis@users.noreply.github.com>
2023-11-17 12:33:50 +0100

    feat(lab_6): task_2

---
+++ lab_6/src/main/java/com/pollub/lab_6/Lab6Application.java
@@ -0,0 +1,13 @@
+package com.pollub.lab_6;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Lab6Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Lab6Application.class, args);
+	}
+
+}
---
+++ lab_6/src/test/java/com/pollub/lab_6/Lab6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pollub.lab_6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
